04fc13a83e77049eed1f77282c26286851644297
marzbana <marza@bu.edu>
2025-02-10 17:29:22 -0500

    v2.9

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'photography',
+    title: 'Alexis Park\'s Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
